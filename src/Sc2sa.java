8522888263dc1d9261258071b11e8edd2a498476
Pires <andrea.pires@etu.univ-amu.fr>
2020-01-31 18:18:42 +0100

    Merci Nicolas tu es notre héros à tous, on te garde au fond de notre coeur.

---
+++ src/Sc2sa.java
@@ -0,0 +1,13 @@
+import sa.SaNode;
+import sc.analysis.DepthFirstAdapter;
+
+public class Sc2sa extends DepthFirstAdapter {
+
+    private SaNode returnValue;
+
+    public SaNode getRoot() {
+        return returnValue;
+    }
+
+
+}

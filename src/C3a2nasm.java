6f5698ed0e5dfac818f385e98c312d076cb8b0c0
JaiPasDidee <andrea.pires@etu.univ-amu.fr>
2020-04-04 14:39:16 +0200

    modif de affect yavait des trucs inutiles

--- src/C3a2nasm.java
+++ src/C3a2nasm.java
@@ -115,10 +115,7 @@ public class C3a2nasm implements C3aVisitor<NasmOperand> {
 
     @Override
     public NasmOperand visit(C3aInstAffect inst) {
-        //affectation a un registre simplement ?
         NasmOperand label = (inst.label != null) ? inst.label.accept(this) : null;
-        NasmRegister reg_eax = nasm.newRegister();
-        reg_eax.colorRegister(Nasm.REG_EAX);
         nasm.ajouteInst(new NasmMov(label, inst.result.accept(this), inst.op1.accept(this), "Affect"));
         return null;
     }

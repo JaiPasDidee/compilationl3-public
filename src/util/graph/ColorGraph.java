234f3907c3d5c2b29356dd72157dff5622c99850
JaiPasDidee <andrea.pires@etu.univ-amu.fr>
2020-04-10 15:13:56 +0200

    mise en forme

--- src/util/graph/ColorGraph.java
+++ src/util/graph/ColorGraph.java
@@ -8,65 +8,63 @@ import java.util.*;
 import java.io.*;
 
 public class ColorGraph {
-    public  Graph          G;
-    public  int            R;
-    public  int            K;
+    public Graph G;
+    public int R;
+    public int K;
     private Stack<Integer> pile;
-    public  IntSet         removed;
-    public  IntSet         spill;
-    public  int[]          couleur;
-    public  Node[]         int2Node;
-    static  int            NOCOLOR = -1;
+    public IntSet removed;
+    public IntSet spill;
+    public int[] couleur;
+    public Node[] int2Node;
+    static int NOCOLOR = -1;
 
-    public ColorGraph(Graph G, int K, int[] phi){
-	this.G       = G;
-	this.K       = K;
-	pile         = new Stack<Integer>(); 
-	R            = G.nodeCount();
-	couleur      = new int[R];
-	removed      = new IntSet(R);
-	spill        = new IntSet(R);
-	int2Node     = G.nodeArray();
-	for(int v=0; v < R; v++){
-	    int preColor = phi[v];
-	    if(preColor >= 0 && preColor < K)
-		couleur[v] = phi[v];
-	    else
-		couleur[v] = NOCOLOR;
-	}
+    public ColorGraph(Graph G, int K, int[] phi) {
+        this.G = G;
+        this.K = K;
+        pile = new Stack<Integer>();
+        R = G.nodeCount();
+        couleur = new int[R];
+        removed = new IntSet(R);
+        spill = new IntSet(R);
+        int2Node = G.nodeArray();
+        for (int v = 0; v < R; v++) {
+            int preColor = phi[v];
+            if (preColor >= 0 && preColor < K)
+                couleur[v] = phi[v];
+            else
+                couleur[v] = NOCOLOR;
+        }
     }
 
     /*-------------------------------------------------------------------------------------------------------------*/
     /* associe une couleur à tous les sommets se trouvant dans la pile */
     /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public void selection()
-    {
+
+    public void selection() {
         //TODO finir la fonction
-        while(!pile.empty()){
+        while (!pile.empty()) {
             int s = pile.pop();
             IntSet c = couleursVoisins(s);
-            if(c.getSize() != K) couleur[s] = choisisCouleur(//C -c);
-        //couleur[s] = choisisCouleur(couleursVoisins(s)); pour remplacer le if ??
+            if (c.getSize() != K) couleur[s] = choisisCouleur(//C -c);
+                    //couleur[s] = choisisCouleur(couleursVoisins(s)); pour remplacer le if ??
         }
     }
-    
+
     /*-------------------------------------------------------------------------------------------------------------*/
     /* récupère les couleurs des voisins de t */
     /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public IntSet couleursVoisins(int t)
-    {
+
+    public IntSet couleursVoisins(int t) {
         IntSet neighbourColors = new IntSet(K);
         Node node = int2Node[t];
         NodeList neighbours = node.adj();
         // Parcours les voisins et ajoute dans neighbourColors les couleurs déjà utilisées par les voisins
         // du noeud dont l'indexe est passé en argument à la fonction
-        for(NodeList q=neighbours; q!=null; q=q.tail) {
+        for (NodeList q = neighbours; q != null; q = q.tail) {
             // Si la couleur est déjà ajoutée au set : continue
-            if(neighbourColors.isMember(couleur[q.head.mykey])){
+            if (neighbourColors.isMember(couleur[q.head.mykey])) {
                 continue;
-            } else{
+            } else {
                 // Aussi non on rajoute la couleur utilisée par le noeud au set de couleur
                 neighbourColors.add(couleur[q.head.mykey]);
             }
@@ -74,33 +72,31 @@ public class ColorGraph {
         }
         return neighbourColors;
     }
-    
+
     /*-------------------------------------------------------------------------------------------------------------*/
     /* recherche une couleur absente de colorSet */
     /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public int choisisCouleur(IntSet colorSet)
-    {
+
+    public int choisisCouleur(IntSet colorSet) {
         //choisit une couleur dans l’ensemble de couleurs ColorSet
         int size = colorSet.getSize();
         for (int i = 0; i < size; i++) {
-            if(!colorSet.isMember(i)) return i;
+            if (!colorSet.isMember(i)) return i;
         }
         //valeur par default cad NOCOLOR
         return -1;
     }
-    
+
     /*-------------------------------------------------------------------------------------------------------------*/
     /* calcule le nombre de voisins du sommet t */
     /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public int nbVoisins(int t)
-    {
+
+    public int nbVoisins(int t) {
         Node node = int2Node[t];
         NodeList neighbours = node.adj();
         int count = 0;
         //Parcours les voisins du noeud passé en argument pour les dénombrer
-        for(NodeList q=neighbours; q!=null; q=q.tail) {
+        for (NodeList q = neighbours; q != null; q = q.tail) {
             count++;
         }
         return count;
@@ -113,28 +109,26 @@ public class ColorGraph {
     /* à la fin du processus, le graphe peut ne pas être vide, il s'agit des temporaires qui ont au moins k voisin */
     /*-------------------------------------------------------------------------------------------------------------*/
 
-    public void simplification()
-    {
+    public void simplification() {
         boolean modif = true;
-        while (pile.size() != R && modif){
+        while (pile.size() != R && modif) {
             modif = false;
-            for (var node: int2Node) {
+            for (var node : int2Node) {
                 if (nbVoisins(node.mykey) < K && couleur[node.mykey] == NOCOLOR) {
-                   pile.push(node.mykey);
-                   removed.add(node.mykey);
-                   modif = true;
+                    pile.push(node.mykey);
+                    removed.add(node.mykey);
+                    modif = true;
                 }
             }
         }
     }
-    
+
     /*-------------------------------------------------------------------------------------------------------------*/
     /*-------------------------------------------------------------------------------------------------------------*/
-    
-    public void debordement()
-    {
+
+    public void debordement() {
         spill = new IntSet(R);
-        while (pile.size() != R){
+        while (pile.size() != R) {
             int s = choisi_sommet();
             pile.push(s);
             removed.add(s);
@@ -143,7 +137,7 @@ public class ColorGraph {
         }
     }
 
-    private int choisi_sommet(){
+    private int choisi_sommet() {
         for (int i = 0; i < removed.getSize(); ++i)
             if (!removed.isMember(i) && couleur[i] == NOCOLOR) return i;
 
@@ -154,21 +148,18 @@ public class ColorGraph {
     /*-------------------------------------------------------------------------------------------------------------*/
     /*-------------------------------------------------------------------------------------------------------------*/
 
-    public void coloration()
-    {
-	this.simplification();
-	this.debordement();
-	this.selection();
+    public void coloration() {
+        this.simplification();
+        this.debordement();
+        this.selection();
     }
 
-    void affiche()
-    {
-	System.out.println("vertex\tcolor");
-	for(int i = 0; i < R; i++){
-	    System.out.println(i + "\t" + couleur[i]);
-	}
+    void affiche() {
+        System.out.println("vertex\tcolor");
+        for (int i = 0; i < R; i++) {
+            System.out.println(i + "\t" + couleur[i]);
+        }
     }
-    
-    
+
 
 }
